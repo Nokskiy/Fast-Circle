474dba998e041aeb1368ea4160067c0d63d0c221
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2026-01-20 18:15:33 +0500

    disable basic window init

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,23 +1,67 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Ikst.KeyboardHook;
 
 namespace FastCircle;
 
 public partial class App : Application
 {
+    private KeyboardHook kh = new();
+    private double _firstButtonPressedTime;
+    
+    private MainWindow _mainWindow;
+    private bool _userUsedIt;
+    
     public override void Initialize()
     {
+        kh.KeyDown += (_, e) =>
+        {
+            var t = DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+            if (e.Key == VirtualKeys.LCONTROL)
+                _firstButtonPressedTime = t;
+            else if (e.Key == VirtualKeys.LMENU)
+                if (t - _firstButtonPressedTime < 200)
+                {
+                    if (_mainWindow == null)
+                    {
+                        _userUsedIt = true;
+                        OnFrameworkInitializationCompleted();
+                    }
+                    SetWindowVisible(!_mainWindow.IsVisible);
+                }
+        };
+        kh.Start();
         AvaloniaXamlLoader.Load(this);
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
+        if(!_userUsedIt) return;
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
+            _mainWindow = new MainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+    
+    private void SetWindowVisible(bool visible)
+    {
+        if (!visible)
+        {
+            _mainWindow.Hide();
+            _mainWindow.Topmost = false;
+        }
+        else
+        {
+            _mainWindow.Show();
+            _mainWindow.Topmost = true;
+            _mainWindow.Width = _mainWindow.Screens.Primary!.Bounds.Width;
+            _mainWindow.Height = _mainWindow.Screens.Primary.Bounds.Height;
+            _mainWindow.Position = new PixelPoint(0, 0);
+        }
     }
 }
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,13 +1,15 @@
 using System;
+using System.Timers;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 using Ikst.KeyboardHook;
 
 namespace FastCircle;
 
 public partial class MainWindow : Window
 {
-    private bool _shown;
+    private bool _shown => IsVisible;
 
     private KeyboardHook kh = new();
     private double _firstButtonPressedTime;
@@ -15,52 +17,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-
-        kh.KeyDown += (_, e) =>
-        {
-            var t = DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
-            if (e.Key == VirtualKeys.LCONTROL)
-                _firstButtonPressedTime = t;
-            else if (e.Key == VirtualKeys.LMENU)
-                if (t - _firstButtonPressedTime < 200)
-                {
-                    _shown = !_shown;
-                    SetWindowVisible(_shown);
-                }
-        };
-
-        kh.Start();
-
-        SetWindowVisible(_shown);
-    }
-
-    private void SetWindowVisible(bool visible)
-    {
-        WindowState = visible ? WindowState.Maximized : WindowState.Minimized;
-        if (!visible)
-        {
-            Position = new PixelPoint(-1000, -1000);
-            Width = 0;
-            Height = 0;
-            return;
-        }
-
-        Width = Screens.Primary!.Bounds.Width;
-        Height = Screens.Primary.Bounds.Height;
     }
 
     #region cheating by Windows system
 
     private void UserTryToResize(object? sender, SizeChangedEventArgs e)
     {
-        if(!_shown) return;
+        if (!_shown) return;
         Width = Screens.Primary!.Bounds.Width;
         Height = Screens.Primary.Bounds.Height;
     }
 
     private void UserTryToChangePosition(object? sender, PixelPointEventArgs e)
     {
-        if(_shown)
+        if (_shown)
             Position = new PixelPoint(0, 0);
     }
 
